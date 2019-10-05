911ae518b70326f78aa093c3150bd4c135ebdd0d
Eugene Bekker <ebekker@users.noreply.github.com>
2019-10-05 10:25:56 -0400

    fixing and cleaning up sample

--- SAMPLE-action.ps1
+++ SAMPLE-action.ps1
@@ -3,17 +3,10 @@
 ##
 ## This is a sample GitHub Action script written in PowerShell Core.
 ## You can write your logic in PWSH to perform GitHub Actions.
+##
 ## You interface with the Actions/Workflow system by interacting
-## with the environment.
-##
-## You get input from environment variables. More info here:
-##    https://help.github.com/en/articles/metadata-syntax-for-github-actions#inputs
-##    https://help.github.com/en/articles/workflow-syntax-for-github-actions#jobsjob_idstepswith
-##    https://help.github.com/en/articles/virtual-environments-for-github-actions#environment-variables
-##
-## You can set output and invoke workflow actions by writing
-## commands to the console output.  More info here:
-##    https://help.github.com/en/articles/development-tools-for-github-actions#logging-commands
+## with the environment.  The `ActionsCore.ps1` library makes this
+## easier and more natural.
 ##
 
 ## Load up some common functionality for interacting
@@ -21,8 +14,8 @@
 . ./lib/ActionsCore.ps1
 
 ## Pull in some inputs
-$salutation = Get-ActionInput saluation -Required
-$audience = Get-ActionInput audience
+$salutation = Get-ActionInput salutation -Required
+$audience   = Get-ActionInput audience
 
 if (-not $salutation) {
     ## We actually specified this input as *required* above so
@@ -37,7 +30,9 @@ $greeting = "$($salutation) $($audience)!"
 
 ## Persist the greeting in the environment for all subsequent steps
 Set-ActionVariable -Name build_greeting -Value greeting
+
 ## Expose the greeting as an output value of this step instance
 Set-ActionOutput -Name greeting -Value $greeting
+
 ## Write it out to the log for good measure
 Write-ActionInfo $greeting
