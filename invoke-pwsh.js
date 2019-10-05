a14e432d4a7f84659c794f66daba11e22cdd58cd
Eugene Bekker <ebekker@users.noreply.github.com>
2019-10-05 10:07:39 -0400

    renaming bootstrap JS script subdir

--- invoke-pwsh.js
+++ invoke-pwsh.js
@@ -4,8 +4,9 @@ const exec = require('@actions/exec');
 
 async function run() {
     try {
-        const pwshScript = `${__dirname}/action.ps1`
-        await exec.exec('pwsh', [ '-c', pwshScript ]);
+        const pwshFolder = __dirname.replace(/\/_init$/, '');
+        const pwshScript = `${pwshFolder}/action.ps1`
+        await exec.exec('pwsh', [ '-f', pwshScript ]);
     } catch (error) {
         core.setFailed(error.message);
     }
